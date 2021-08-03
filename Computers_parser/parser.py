a57a5a34e35eae5b4331fd1b617ff62fb7046684
Your Name <you@example.com>
2021-08-04 00:13:36 +0600

    для корректности отображения папок

--- Computers_parser/parser.py
+++ Computers_parser/parser.py
@@ -143,7 +143,7 @@ def get_data():
 
 @check_time
 def facad_parser():
-	# get_pages("https://www.xcomspb.ru/search/kompyuter_i_kompyuternaya_platforma/?o=n&s=%D0%9A%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80")
+	get_pages("https://www.xcomspb.ru/search/kompyuter_i_kompyuternaya_platforma/?o=n&s=%D0%9A%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80")
 	get_data()
 
 if __name__ == "__main__":
