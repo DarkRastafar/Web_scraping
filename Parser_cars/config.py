12389fad659d8e2bb746968978e459fbe3243818
Your Name <you@example.com>
2022-04-19 15:18:09 +0600

    "refactoring cars parser"

---
+++ Parser_cars/config.py
@@ -0,0 +1,5 @@
+URL = 'https://auto.ria.com/newauto/marka-jeep/'
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+    'accept': '*/*'}
+HOST = 'https://auto.ria.com'
